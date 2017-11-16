ebbdc0fd67e37690afdbc6bc12968e5885c460b9
Thúlio Costa <thulio@thuliocosta.com>
2017-11-16 18:33:56 -0200

    Ignore server/index.js coverage

--- src/server/index.js
+++ src/server/index.js
@@ -2,10 +2,13 @@
 
 /* eslint-disable no-console */
 
+/* istanbul ignore next */
 const app = require("./app");
 
+/* istanbul ignore next */
 const PORT = process.env.RESTQL_MANAGER_PORT || 3000;
 
+/* istanbul ignore next */
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
